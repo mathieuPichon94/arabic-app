24d55db0781402c8e93fb047b31cde4e28d39ac4
Mathieu Pichon <mathieu@ubble.ai>
2022-12-28 16:04:52 +0100

    add trpc

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,14 @@
 import { Card } from "@/components/Card";
+import { trpc } from "@/utils/trpc";
 
 const btn =
   "inline-flex items-center px-3 py-1.5 border border-gray-300 shadow-sm font-medium rounded-full text-gray-700 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500";
 
 export default function Home() {
+  const hello = trpc.hello.useQuery({ text: "client" });
+  if (!hello.data) {
+    return <div>Loading...</div>;
+  }
   return (
     <div className="h-screen w-screen flex flex-col justify-around items-center relative">
       <h1 className="text-3xl font-bold underline">Welcome to arabice test</h1>
