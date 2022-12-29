98d51872cc5aeed9932b81b459d3620a10a8771e
Mathieu Pichon <mathieu@ubble.ai>
2022-12-29 10:24:08 +0100

    fix card design

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -13,46 +13,44 @@ export const Card: React.FC<CardProps> = ({ wordsToTest }) => {
   const wordToTest = wordsToTest[indexWord];
 
   return (
-    <div className="flex justify-center w-screen">
-      <div className="block p-6 rounded-lg shadow-lg bg-white w-1/3">
-        <h5 className="text-gray-900 text-xl leading-tight font-medium mb-2 text-center pb-10">
-          Translate the word
-        </h5>
-        <div className="flex justify-between">
-          <p className="text-gray-700 text-base mb-4">{wordToTest.french}</p>
-          {showWord && (
-            <p className="text-gray-700 text-base mb-4">{wordToTest.arab}</p>
-          )}
-        </div>
-        <div className="p-2 flex flex-row justify-around">
-          <Button
-            text={"Previous"}
-            onClickButton={() => {
-              if (indexWord > 0) {
-                setIndexWord(indexWord - 1);
-                setShowWord(true);
-              }
-            }}
-            intent={"danger"}
-          />
-          <Button
-            text={"Show"}
-            onClickButton={() => {
+    <div className="block p-4 rounded-lg shadow-lg bg-white w-11/12 md:w-1/3 lg:w-1/3">
+      <h5 className="text-gray-900 text-xl leading-tight font-medium mb-2 text-center pb-10">
+        Translate the word
+      </h5>
+      <div className="flex justify-between pb-10">
+        <p className="text-gray-700 text-base">{wordToTest.french}</p>
+        {showWord && (
+          <p className="text-gray-700 text-base">{wordToTest.arab}</p>
+        )}
+      </div>
+      <div className="flex justify-around">
+        <Button
+          text={"Previous"}
+          onClickButton={() => {
+            if (indexWord > 0) {
+              setIndexWord(indexWord - 1);
               setShowWord(true);
-            }}
-            intent={"primary"}
-          />
+            }
+          }}
+          intent={"danger"}
+        />
+        <Button
+          text={"Show"}
+          onClickButton={() => {
+            setShowWord(true);
+          }}
+          intent={"primary"}
+        />
 
-          <Button
-            text={"Next"}
-            onClickButton={() => {
-              setShowWord(!showWord);
-              setIndexWord(indexWord + 1);
-            }}
-            intent={showWord ? "success" : "disabled"}
-            disabled={!showWord}
-          />
-        </div>
+        <Button
+          text={"Next"}
+          onClickButton={() => {
+            setShowWord(!showWord);
+            setIndexWord(indexWord + 1);
+          }}
+          intent={showWord ? "success" : "disabled"}
+          disabled={!showWord}
+        />
       </div>
     </div>
   );
