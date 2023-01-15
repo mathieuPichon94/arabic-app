7738317fe11f873a793a538af755b10ff40c00f7
Mathieu Pichon <mathieu@ubble.ai>
2023-01-15 20:53:22 +0100

    feat(app): add search tool

---
+++ src/components/NewWordListing.tsx
@@ -0,0 +1,69 @@
+import { Box, Checkbox, Grid, TextInput, createStyles } from "@mantine/core";
+import { useDebouncedValue } from "@mantine/hooks";
+import dayjs from "dayjs";
+import { DataTable } from "mantine-datatable";
+import React, { useEffect, useState } from "react";
+import { IconSearch } from "@tabler/icons";
+
+const useStyles = createStyles((theme) => ({
+  root: {
+    height: "100%",
+  },
+}));
+type wordToTest = { arab: string; french: string };
+
+export const SearchingAndFilteringExample: React.FC<{
+  wordsToTest: wordToTest[];
+}> = ({ wordsToTest }) => {
+  const [records, setRecords] = useState(wordsToTest);
+
+  const [query, setQuery] = useState("");
+  const [veteransOnly, setVeteransOnly] = useState(false);
+  const [debouncedQuery] = useDebouncedValue(query, 200);
+
+  useEffect(() => {
+    setRecords(
+      wordsToTest.filter(({ arab, french }) => {
+        return !(
+          debouncedQuery !== "" &&
+          !french.toLowerCase().includes(debouncedQuery.trim().toLowerCase())
+        );
+      })
+    );
+  }, [debouncedQuery, wordsToTest]);
+  const { classes } = useStyles();
+  return (
+    <>
+      <Grid align="center">
+        <Grid.Col>
+          <TextInput
+            sx={{ flexBasis: "60%" }}
+            placeholder="Search french words..."
+            icon={<IconSearch size={16} />}
+            value={query}
+            onChange={(e) => setQuery(e.currentTarget.value)}
+          />
+        </Grid.Col>
+        <Grid.Col xs={4} sm={3}>
+          <Checkbox
+            label="Only verbs"
+            checked={veteransOnly}
+            onChange={(e) => setVeteransOnly(e.currentTarget.checked)}
+          />
+        </Grid.Col>
+      </Grid>
+      <Box sx={{ height: "100%" }}>
+        <DataTable
+          withBorder
+          records={records}
+          columns={[
+            {
+              accessor: "french",
+            },
+            { accessor: "arab" },
+          ]}
+        />
+      </Box>
+    </>
+  );
+};
--- src/pages/WordsList.tsx
+++ src/pages/WordsList.tsx
@@ -1,27 +1,14 @@
 import type { GetServerSideProps } from "next";
 
 import { openArabicVoc } from "@/server/utils";
-import { WordListing } from "@/components/WordListing";
 import React from "react";
+import { SearchingAndFilteringExample } from "@/components/NewWordListing";
 
 type wordToTest = { arab: string; french: string };
 const WordsList: React.FC<{
   wordsToTest: wordToTest[];
 }> = ({ wordsToTest }) => {
-  return (
-    <div className="w-screen flex flex-col justify-around items-center">
-      <h1 className="text-2xl font-bold text-center">Vocabulary List</h1>
-      {wordsToTest.map((el) => {
-        return (
-          <WordListing
-            frenchWord={el.french}
-            arabicWord={el.arab}
-            key={el.french}
-          />
-        );
-      })}
-    </div>
-  );
+  return <SearchingAndFilteringExample wordsToTest={wordsToTest} />;
 };
 
 export const getStaticProps: GetServerSideProps = async () => {
